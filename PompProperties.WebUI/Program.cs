426af27eb9bcc143bc71600abd8e24ca7ba00dd1
Marius <maas0903@gmail.com>
2024-03-11 16:20:00 +0100

    also sortof working

--- PompProperties.WebUI/Program.cs
+++ PompProperties.WebUI/Program.cs
@@ -2,39 +2,56 @@ using PompProperties.WebUI.Components;
 using Syncfusion.Blazor;
 using PompProperties.Application;
 using PompProperties.Infrastructure;
-//using PompProperties.Domain;
+using System;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
-builder.Services.AddSyncfusionBlazor();
-
-//Add project layers
-builder.Services.AddApplication();
-builder.Services.AddInfrastructure();
-//builder.Services.AddDomain();
-
-var app = builder.Build();
-
-Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MzA5NTcyNEAzMjM0MmUzMDJlMzBuM0Ruc2NpRkNUaEgveVh3NVFEL3M1aFFHR0dvVEFCbFpRQ3NDa0ZXZmRBPQ==");
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+try
 {
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+
+    var builder = WebApplication.CreateBuilder(args);
+
+	// Add services to the container.
+	builder.Services.AddControllers();
+	builder.Services.AddEndpointsApiExplorer();
+
+	builder.Services.AddRazorComponents()
+		.AddInteractiveServerComponents();
+	builder.Services.AddSyncfusionBlazor();
+
+	//Add project layers
+	builder.Services.AddApplication();
+	builder.Services.AddInfrastructure();
+    //builder.Services.AddDomain();
+
+    builder.Services.AddLogging(loggingBuilder =>
+    {
+        loggingBuilder.ClearProviders();
+    });
+
+    var app = builder.Build();
+
+	Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MzA5NTcyNEAzMjM0MmUzMDJlMzBuM0Ruc2NpRkNUaEgveVh3NVFEL3M1aFFHR0dvVEFCbFpRQ3NDa0ZXZmRBPQ==");
+
+	// Configure the HTTP request pipeline.
+	if (!app.Environment.IsDevelopment())
+	{
+		app.UseExceptionHandler("/Error", createScopeForErrors: true);
+	}
+
+	//add HTTP Client
+
+	app.UseStaticFiles();
+	app.UseAntiforgery();
+	app.MapRazorComponents<App>()
+		.AddInteractiveServerRenderMode();
+	app.MapControllers();
+
+	app.Run();
+
 }
-
-//add HTTP Client
-
-app.UseStaticFiles();
-app.UseAntiforgery();
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
-app.MapControllers();
-
-app.Run();
+catch (Exception exception)
+{
+    throw;
+}
+finally
+{
+}
