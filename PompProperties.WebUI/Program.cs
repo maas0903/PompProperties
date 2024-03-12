f3310e80a47964697cc3a6e48e0bb06068fbc746
Marius <maas0903@gmail.com>
2024-03-12 12:59:56 +0100

    fixes

--- PompProperties.WebUI/Program.cs
+++ PompProperties.WebUI/Program.cs
@@ -1,25 +1,24 @@
-using PompProperties.WebUI.Components;
-using Syncfusion.Blazor;
 using PompProperties.Application;
 using PompProperties.Infrastructure;
-using System;
+using PompProperties.WebUI.Components;
+using Syncfusion.Blazor;
 
 try
 {
 
     var builder = WebApplication.CreateBuilder(args);
 
-	// Add services to the container.
-	builder.Services.AddControllers();
-	builder.Services.AddEndpointsApiExplorer();
+    // Add services to the container.
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
 
-	builder.Services.AddRazorComponents()
-		.AddInteractiveServerComponents();
-	builder.Services.AddSyncfusionBlazor();
+    builder.Services.AddRazorComponents()
+        .AddInteractiveServerComponents();
+    builder.Services.AddSyncfusionBlazor();
 
-	//Add project layers
-	builder.Services.AddApplication();
-	builder.Services.AddInfrastructure();
+    //Add project layers
+    builder.Services.AddApplication();
+    builder.Services.AddInfrastructure();
     //builder.Services.AddDomain();
 
     builder.Services.AddLogging(loggingBuilder =>
@@ -29,23 +28,23 @@ try
 
     var app = builder.Build();
 
-	Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MzA5NTcyNEAzMjM0MmUzMDJlMzBuM0Ruc2NpRkNUaEgveVh3NVFEL3M1aFFHR0dvVEFCbFpRQ3NDa0ZXZmRBPQ==");
+    Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MzA5NTcyNEAzMjM0MmUzMDJlMzBuM0Ruc2NpRkNUaEgveVh3NVFEL3M1aFFHR0dvVEFCbFpRQ3NDa0ZXZmRBPQ==");
 
-	// Configure the HTTP request pipeline.
-	if (!app.Environment.IsDevelopment())
-	{
-		app.UseExceptionHandler("/Error", createScopeForErrors: true);
-	}
+    // Configure the HTTP request pipeline.
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    }
 
-	//add HTTP Client
+    //add HTTP Client
 
-	app.UseStaticFiles();
-	app.UseAntiforgery();
-	app.MapRazorComponents<App>()
-		.AddInteractiveServerRenderMode();
-	app.MapControllers();
+    app.UseStaticFiles();
+    app.UseAntiforgery();
+    app.MapRazorComponents<App>()
+        .AddInteractiveServerRenderMode();
+    app.MapControllers();
 
-	app.Run();
+    app.Run();
 
 }
 catch (Exception exception)
